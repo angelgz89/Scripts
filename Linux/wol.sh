e71851e6c48e846dda0ad5efa2685a5bb348e027
angelgz89 <agz2712@gmail.com>
2021-11-08 17:31:26 +0100

    wol

--- Linux/wol.sh
+++ Linux/wol.sh
@@ -1,12 +1,20 @@
-#!/bin/bash
+#! /bin/bash
 
 #sudo apt install ethtool -y
 #sudo ethtool -s INTERFACE wol g
 
-echo "Quieres despertar Macro server? 1"
+echo "Quieres despertar Macro server? (s/n))"
 read a
 
-if [ $a = "1" ];
+if [ $a = "s" ];
 then
 	wakeonlan -i 192.168.1.101 -p 9 FC:34:97:66:DE:0E
+fi
+
+echo "Quieres despertar MiniPc? (s/n))"
+read b
+
+if [ $b = "s" ];
+then
+	wakeonlan -i 192.168.1.111 -p 9 1C:83:41:28:1C:6E
 fi
