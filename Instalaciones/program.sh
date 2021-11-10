4742ae6029b87ac3380f2cd7164f6af1cce73240
angelgz89 <agz2712@gmail.com>
2021-11-10 16:44:07 +0100

    cambios

--- Instalaciones/program.sh
+++ Instalaciones/program.sh
@@ -128,6 +128,40 @@ function PIP ()
 	sudo pip3 install paho-mqtt
 }
 
+function configuraWOL ()
+{
+    echo interfaz?
+    read interfaz
+
+    echo "Por favor, introduce la contraseña de administrador para usar el programa:"
+    read -s cont
+
+    echo $cont | sudo -S apt update -y
+    sudo apt install ethtool -y
+    sudo ethtool -s $interfaz wol g
+    sudo apt install wakeonlan -y
+
+    touch wol.service
+
+    echo '[Unit]
+    Description=Configure Wake On LAN
+
+    [Service]
+    Type=oneshot
+    ExecStart=/sbin/ethtool -s '$interfaz' wol g
+
+    [Install]
+    WantedBy=basic.target' >> wol.service
+
+    sudo cp wol.service /etc/systemd/system/
+
+    rm -R wol.service
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable wol.service
+    sudo systemctl start wol.service
+}
+
 function flaspak ()
 {
     sudo apt install flatpak
