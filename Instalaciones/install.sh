f6a256b62902917b25f0fd05cb31547fe11b52a9
angelgz89 <agz2712@gmail.com>
2021-11-11 19:02:14 +0100

    cambios

--- Instalaciones/install.sh
+++ Instalaciones/install.sh
@@ -4,22 +4,25 @@
 # Author: M4h0uT
 
 #Colours
-greenColour="\e[0;32m\033[1m"
+verde="\e[0;32m\033[1m"
 endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+rojo="\e[0;31m\033[1m"
+turquesa="\e[0;34m\033[1m"
+amarillo="\e[0;33m\033[1m"
+lila="\e[0;35m\033[1m"
+turquesa="\e[0;36m\033[1m"
+gris="\e[0;37m\033[1m"
 
 
 
-function programa ()
+function login ()
 {
-    bash <(wget -qO- https://raw.githubusercontent.com/angelgz89/Scripts/master/Linux/$1.sh)
+    clear
+    echo -e "${lila}Por favor, introduce la contraseña de administrador: ${endColour}"
+    read -s cont
+    clear
 }
 
-echo a?
-read a
-programa $a
+#########################
+
+login
