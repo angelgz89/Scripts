79318786926ba39d639de874ee0432c9544adf08
angelgz89 <agz2712@gmail.com>
2021-11-26 14:52:22 +0100

    cambios

--- Linux/entradasssh.sh
+++ Linux/entradasssh.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-sudo cat /var/log/auth* | grep -a "Accepted" | awk '{print $1 " " $2 "\t" $3 "\t" $11 "\t" $9 }'
+sudo cat /var/log/auth.log | grep -a "Accepted" | awk '{print $1 " " $2 "\t" $3 "\t" $11 "\t" $9 }'
