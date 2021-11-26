9f65ec35b33420368539bccaff3a8ac541344c63
angelgz89 <agz2712@gmail.com>
2021-11-26 16:44:34 +0000

    cambios

--- Linux/filtroip.sh
+++ Linux/filtroip.sh
@@ -3,7 +3,9 @@
 #sudo ufw enable
 #sudo ufw default allow incoming
 
-sudo cat /var/log/auth.log | grep -a "Failed password" | awk '{print $11}' | uniq -c | sort -nr | awk '{print $2}' | grep -E '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?(\.|$)){4}\b' | uniq > ipsbloqueadas.txt
+sudo cat /var/log/auth.log | grep -a "Failed password" | awk '{print $11}' | uniq -c | sort -nr | awk '{print $2}' | grep -E '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?(\.|$)){4}\b' | uniq >> ipsbloqueadas.txt
+sudo cat /var/log/auth.log | grep -a "Failed" | awk '{print $13}' | uniq -c | sort -nr | awk '{print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" >> ipsbloqueadas.txt
+sudo cat /var/log/auth.log | grep -a "Did not" | awk '{print $12}' | uniq -c | sort -nr | awk '{print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" >> ipsbloqueadas.txt
 
 for ip in $(cat ipsbloqueadas.txt);
 do
