957e09a5b727848ee8d7e4e9f77c1b7e30a9b9c0
angelgz89 <agz2712@gmail.com>
2021-12-02 16:40:07 +0100

    cambios

--- Linux/ZSH.sh
+++ Linux/ZSH.sh
@@ -33,5 +33,4 @@ then
     prompt_context() {
     if [[ "$USER" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
         prompt_segment black default "%(!.%{%F{yellow}%}.)$USER"
-    fi
-    ' > .zshrc
+    fi' > .zshrc
