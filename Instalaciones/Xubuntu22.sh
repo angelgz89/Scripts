bf3f7a9e78cfc062df3f5c7aacbbe44b1dca4d77
Gutierrez Zapata, Angel <agutierrezz@sia.es>
2022-04-23 18:55:17 +0200

    pruebas

--- Instalaciones/Xubuntu22.sh
+++ Instalaciones/Xubuntu22.sh
@@ -175,7 +175,7 @@ function ZSH()
     zsh-syntax-highlighting
     )
     source $ZSH/oh-my-zsh.sh
-    export PATH="$PATH:$HOME/Scripts/Linux"' > .zshrc
+    export PATH="$PATH:$HOME/Scripts/Linux"' > ~/.zshrc
 
     #zsh -l
     echo $cont | chsh -s $(which zsh)
