de94407815deecb78a6a0d26ffeb4b3aa9122cdc
Gutierrez Zapata, Angel <agutierrezz@sia.es>
2022-04-26 12:44:32 +0200

    pruebas

--- Linux/Firewall/filtroip.sh
+++ Linux/Firewall/filtroip.sh
@@ -3,9 +3,9 @@
 #sudo ufw enable
 #sudo ufw default allow incoming
 
-sudo cat /var/log/auth.log | grep -a "Failed password" | awk '{print $11}' | uniq -c | sort -nr | awk '{print $2}' | grep -E '\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?(\.|$)){4}\b' | uniq >> ipsbloqueadas.txt
-sudo cat /var/log/auth.log | grep -a "Failed" | awk '{print $13}' | uniq -c | sort -nr | awk '{print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" >> ipsbloqueadas.txt
-sudo cat /var/log/auth.log | grep -a "Did not" | awk '{print $12}' | uniq -c | sort -nr | awk '{print $2}' | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" >> ipsbloqueadas.txt
+sudo cat /var/log/auth.log | grep -a "Failed password" | awk '{print $11}' | un>
+sudo cat /var/log/auth.log | grep -a "Failed" | awk '{print $13}' | uniq -c | s>
+sudo cat /var/log/auth.log | grep -a "Did not" | awk '{print $12}' | uniq -c | >
 
 for ip in $(cat ipsbloqueadas.txt);
 do
@@ -14,5 +14,4 @@ do
 done
 
 cat ipsbloqueadas.txt | uniq >> IPbloqueadas.txt
-#cat IPbloqueadas.txt | uniq >> IPbloqueadas.txt
 rm ipsbloqueadas.txt
